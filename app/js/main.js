fe074919e1294955535f24d76888d34e5d2abd13
Raman <raman.yasel@yahoo.com>
2017-06-18 22:27:22 +0300

    modal windows

--- app/js/main.js
+++ app/js/main.js
@@ -2,15 +2,14 @@ $(document).ready(function($) {
 
 	// - mobile menu
 	$body = $("body");
-	$menuTrigger = $("#menu__trigger");
 
-	$menuTrigger.on("click", function() {
+	$("#menu__trigger, .header__menu-list a").on("click", function() {
 		if ($body.hasClass("menu__open")) {
 			$body.removeClass("menu__open");
-			$(this).removeClass("active__mod");
+			$("#menu__trigger").removeClass("active__mod");
 		} else {
 			$body.addClass("menu__open");
-			$(this).addClass("active__mod");
+			$("#menu__trigger").addClass("active__mod");
 		}
 	});
 
@@ -27,21 +26,42 @@ $(document).ready(function($) {
 
 	// fixed header
 	$(window).scroll(function() {
-			var sticky = $(".header"),
-				scroll = $(window).scrollTop();
+		var sticky = $(".header"),
+			scroll = $(window).scrollTop();
 
-			if (scroll >= 40) sticky.addClass("header--fixed");
-			else sticky.removeClass("header--fixed");
-		});
+		if (scroll >= 40) sticky.addClass("header--fixed");
+		else sticky.removeClass("header--fixed");
+	});
 
-		// slider`s
-		var slider1 = new Swiper(".swiper-hero", {
-			pagination: ".swiper-pagination",
-			nextButton: ".swiper-button-next",
-			prevButton: ".swiper-button-prev",
-			paginationClickable: true,
-			loop: true
+	// - request button
+	$(".request").click(function() {
+		$("#modal_request").addClass("modal--show");
+		$(".modal-bg").css("display", "block");
+	});
+
+	$(".modal__close, .modal-bg").click(function() {
+		$("#modal_request").removeClass("modal--show");
+		$(".modal-bg").css("display", "none");
+	});
+
+	// date by w2ui
+	$(function() {
+		var month = (new Date()).getMonth() + 1;
+		var year = (new Date()).getFullYear();
+
+		$("input[type=eu-date]").w2field("date", {
+			format: "d.m.yyyy"
 		});
+	});
+
+	// slider`s
+	var slider1 = new Swiper(".swiper-hero", {
+		pagination: ".swiper-pagination",
+		nextButton: ".swiper-button-next",
+		prevButton: ".swiper-button-prev",
+		paginationClickable: true,
+		loop: true
+	});
 
 	var slider2 = new Swiper(".swiper-reviews", {
 		pagination: ".swiper-pagination",
@@ -63,5 +83,34 @@ $(document).ready(function($) {
 			}
 		}
 	});
+
+	// fancybox
+	$("#details-click").fancybox({
+		autoSize: true,
+		closeClick: true,
+		openEffect: "fade",
+		closeEffect: "fade-out"
+	});
+
+	// fancy gallery
+	var galleryTop = new Swiper(".gallery-top", {
+		nextButton: ".swiper-button-next",
+		prevButton: ".swiper-button-prev",
+		spaceBetween: 0,
+		slidesPerView: 1
+	});
+
+	var galleryThumbs = new Swiper(".gallery-thumbs", {
+		nextButton: ".swiper-button-next",
+		prevButton: ".swiper-button-prev",
+		spaceBetween: 10,
+		centeredSlides: true,
+		slidesPerView: "8",
+		touchRatio: 0.2,
+		slideToClickedSlide: true
+	});
+
+	galleryTop.params.control = galleryThumbs;
+	galleryThumbs.params.control = galleryTop;
 
 });
